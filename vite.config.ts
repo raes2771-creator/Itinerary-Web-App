388711c6caa0017bf6b225ab3c034a4f3d3ecd77
Stanley Smith <stansmith2501@gmail.com>
2025-12-21 21:32:29 +0000

    Adding template Vite app with Chakra UI components

---
+++ vite.config.ts
@@ -0,0 +1,14 @@
+import react from "@vitejs/plugin-react"
+import { resolve } from "path"
+import { defineConfig } from "vite"
+import tsconfigPaths from "vite-tsconfig-paths"
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [react(), tsconfigPaths()]
+  // resolve: {
+  //   alias: {
+  //     "@chakra-ui/react": resolve("..", "..", "packages/react/src"),
+  //   },
+  // },
+})
